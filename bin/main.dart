8e09f98992cd1a7abaf6c4df2de598cff94e390c
Nathanial Fine <nathanial.fine@gmail.com>
2018-07-29 11:29:52 -0400

    Runs on the number of cpus available. Will probably put into a config file at some point

--- bin/main.dart
+++ bin/main.dart
@@ -1,11 +1,12 @@
 import 'package:aaas_backend/aaas_backend.dart';
+import 'dart:io' show Platform;
 
 Future main() async {
   var app = new Application<AaasBackendSink>()
       ..configuration.configurationFilePath = "config.yaml"
       ..configuration.port = 8000;
 
-  await app.start(numberOfInstances: 2);
+  await app.start(numberOfInstances: Platform.numberOfProcessors);
 
   print("Application started on port: ${app.configuration.port}.");
   print("Use Ctrl-C (SIGINT) to stop running the application.");
