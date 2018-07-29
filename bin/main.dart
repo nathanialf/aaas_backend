5040efd9817fc0b3371c0ed4808ae91255d0ebe6
Nathanial Fine <nathanial.fine@gmail.com>
2018-07-29 11:22:18 -0400

    First commit, just the default aqueduct project at the moment.

---
+++ bin/main.dart
@@ -0,0 +1,12 @@
+import 'package:aaas_backend/aaas_backend.dart';
+
+Future main() async {
+  var app = new Application<AaasBackendSink>()
+      ..configuration.configurationFilePath = "config.yaml"
+      ..configuration.port = 8000;
+
+  await app.start(numberOfInstances: 2);
+
+  print("Application started on port: ${app.configuration.port}.");
+  print("Use Ctrl-C (SIGINT) to stop running the application.");
+}
---
+++ lib/aaas_backend.dart
@@ -0,0 +1,11 @@
+/// aaas_backend
+///
+/// A Aqueduct web server.
+library aaas_backend;
+
+export 'dart:async';
+export 'dart:io';
+
+export 'package:aqueduct/aqueduct.dart';
+
+export 'aaas_backend_sink.dart';
